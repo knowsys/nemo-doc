65b3513291b088bdd5b279f2a3193f9f61782e5c
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-05-14 20:20:05 +0200

    Bump astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,58 +45,60 @@ export default defineConfig({
   site: "https://knowsys.github.io/",
   base,
   outDir: "./dist/nemo-doc",
-  integrations: [starlight({
-    title: "Nemo",
-    customCss: [
-      "./src/tailwind.css",
-      "./src/styles/custom.css",
-      // Fontsource files for to regular and semi-bold font weights.
-      "@fontsource/comfortaa/400.css",
-      "@fontsource/comfortaa/600.css",
-    ],
-    favicon: "/favicon.svg",
-    social: {
-      github: "https://github.com/knowsys/nemo",
-    },
-    components: {
-      SiteTitle: "./src/components/SiteTitle.astro",
-    },
-    sidebar: [
-      {
-        label: "Introduction",
-        items: [
-          {label: "Introducing Nemo", slug: "intro/welcome"},
-          {label: "Ways of using Nemo", slug: "intro/usage"},
-          {label: "Rule Language", slug: "intro/tour"},
-          {label: "Nemo examples", slug: "intro/examples"},
-          {label: "Nemo research", slug: "intro/research"},
-
-        ]
+  integrations: [
+    starlight({
+      title: "Nemo",
+      customCss: [
+        "./src/tailwind.css",
+        "./src/styles/custom.css",
+        // Fontsource files for to regular and semi-bold font weights.
+        "@fontsource/comfortaa/400.css",
+        "@fontsource/comfortaa/600.css",
+      ],
+      favicon: "/favicon.svg",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/knowsys/nemo",
+        },
+      ],
+      components: {
+        SiteTitle: "./src/components/SiteTitle.astro",
       },
-      {
-        label: "Installation and usage",
-        items: [
-          // Each item here is one entry in the navigation menu.
-          { label: "Nemo in the browser", slug: "installation/browser" },
-          { label: "Installation", slug: "installation/install" },
-          { label: "Command Line", slug: "installation/cli" },
-          { label: "Wasm Integration", slug: "installation/wasm" },
-          { label: "Python API", slug: "installation/python" },
-          { label: "Rust API", slug: "installation/rust" },
-
-        ],
-      },
-      {
-        label: "Language Reference",
-        items: [
-          { label: "Import", slug: "reference/imports" },
-          { label: "Export", slug: "reference/exports" },
-          { label: "Builtin Functions", slug: "reference/builtins" },
-          { label: "Datatypes", slug: "reference/datatypes" },
-          { label: "Attributes", slug: "reference/attributes" },
-          { label: "Aggregates", slug: "reference/aggregates" },
-
-          ]
+      sidebar: [
+        {
+          label: "Introduction",
+          items: [
+            { label: "Introducing Nemo", slug: "intro/welcome" },
+            { label: "Ways of using Nemo", slug: "intro/usage" },
+            { label: "Rule Language", slug: "intro/tour" },
+            { label: "Nemo examples", slug: "intro/examples" },
+            { label: "Nemo research", slug: "intro/research" },
+          ],
+        },
+        {
+          label: "Installation and usage",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Nemo in the browser", slug: "installation/browser" },
+            { label: "Installation", slug: "installation/install" },
+            { label: "Command Line", slug: "installation/cli" },
+            { label: "Wasm Integration", slug: "installation/wasm" },
+            { label: "Python API", slug: "installation/python" },
+            { label: "Rust API", slug: "installation/rust" },
+          ],
+        },
+        {
+          label: "Language Reference",
+          items: [
+            { label: "Import", slug: "reference/imports" },
+            { label: "Export", slug: "reference/exports" },
+            { label: "Builtin Functions", slug: "reference/builtins" },
+            { label: "Datatypes", slug: "reference/datatypes" },
+            { label: "Attributes", slug: "reference/attributes" },
+            { label: "Aggregates", slug: "reference/aggregates" },
+          ],
         },
       ],
     }),
@@ -126,11 +128,11 @@ export default defineConfig({
     ],
     resolve: {
       alias: {
-        '@assets': '/src/assets',
-        '@lib': '/src/lib',
-        '@components': '/src/components',
-        '@layouts': '/src/layouts',
-        '@pages': '/src/pages',
+        "@assets": "/src/assets",
+        "@lib": "/src/lib",
+        "@components": "/src/components",
+        "@layouts": "/src/layouts",
+        "@pages": "/src/pages",
       },
     },
   },
